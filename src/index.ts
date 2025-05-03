9343491fbf125a96e212d81d7459669468189873
Mh-Monzil <monzil246@gmail.com>
2025-05-03 22:23:10 +0600

    task 4

--- src/index.ts
+++ src/index.ts
@@ -80,3 +80,23 @@ type Person = {
 
 //
 //
+
+// Task 4: Union and Intersection Types
+
+interface Book {
+  title: string;
+  author: string;
+  pages: number;
+}
+
+interface Magazine {
+  title: string;
+  issue: number;
+  publisher: string;
+}
+
+type ReadingMaterial = Book | Magazine;
+type BookMagazine = Book & Magazine;
+
+//
+//
