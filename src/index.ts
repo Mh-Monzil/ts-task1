92f46da80ef3ba5e3a7ea79aa3ae170b72edc860
Mh-Monzil <monzil246@gmail.com>
2025-05-03 21:45:34 +0600

    task 2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-// 1. Basic Data Types and First Program
+// Task 1: Basic Data Types and First Program
 
 const welcomeMessage: string =
   "Hello World, I will complete this course successfully and become a Next level Web Developer!";
 
 console.log(welcomeMessage);
+
+//
+//
+
+// Task 2: Functions, Optional, and Literal Types
+
+type role = "admin" | "user" | "guest";
+
+const user = (name: string, age: number, role?: role) => {
+  console.log(name, age);
+};
+
+user("Hassan", 20, "admin");
