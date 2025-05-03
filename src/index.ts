130c5ea3f2458adab9257fb2a410b6f58913fb82
Mh-Monzil <monzil246@gmail.com>
2025-05-03 21:21:37 +0600

    task 1

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// 1. Basic Data Types and First Program
+
+const welcomeMessage: string =
+  "Hello World, I will complete this course successfully and become a Next level Web Developer!";
+
+console.log(welcomeMessage);
